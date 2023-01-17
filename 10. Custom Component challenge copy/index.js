288be335e6bba4f54986788b9c955d18fedfe1bf
Peter-TMK <tundepeter01@gmail.com>
2023-01-17 12:13:29 +0100

    new: custom component challenge 2

---
+++ 10. Custom Component challenge copy/index.js
@@ -0,0 +1,32 @@
+// Challenge:
+// Part 2: 
+// - Add a 'header' element with a nested 'nav'. Inside the 'nav',
+//     include an 'img' element with the image of the React logo.
+// - Add an 'h1' with some text describing the page(like 'Reasons I love React')
+// - Place it above the ordered list.
+// - Add a 'footer' after the list that says:
+//     "c 2023 Wey development. All rights reserved."
+
+
+
+function MyReact(){
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src='../7. Mark-up project\react_logo.png' width='70px'></img>
+                </nav>
+            </header>
+            <h1>
+                Reasons I love React
+            </h1>
+            <ol>
+                <li>React is fun</li>
+                <li>React makes frontend easy</li>
+                <li>React is necessary for integrating with the backend in an app</li>
+                <li>For jobs in software engineering</li>
+            </ol>
+        </div>
+    )
+}
+ReactDOM.render(<MyReact />, document.querySelector('#root'))
